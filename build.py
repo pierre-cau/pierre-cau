9d592c0e7f7621cab0f89554c430933ebd8655a6
pierre-cau <pcaupro@gmail.com>
2023-07-11 13:38:12 +0200

    Update Pierre

--- build.py
+++ build.py
@@ -62,6 +62,7 @@ if __name__ == "__main__":
             "![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=JavaScript&logoColor=black)",
             "![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=HTML5&logoColor=white)",
             "![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=CSS3&logoColor=white)",
+            "![Flutter](https://img.shields.io/badge/Flutter-02569B?style=for-the-badge&logo=Flutter&logoColor=white)",
         ],
         "mykofi": {
             "url": "https://ko-fi.com/pierre_cau",
